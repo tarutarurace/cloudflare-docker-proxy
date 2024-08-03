b7e32f1f064696aab1c6e01c0c25486281d286b0
20in20 <48176995+20in20@users.noreply.github.com>
2024-08-04 00:24:12 +0800

    index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.tricej.us.kg": dockerHub,
+  "quay.tricej.us.kg": "https://quay.io",
+  "gcr.tricej.us.kg": "https://gcr.io",
+  "k8s-gcr.tricej.us.kg": "https://k8s.gcr.io",
+  "k8s.tricej.us.kg": "https://registry.k8s.io",
+  "ghcr.tricej.us.kg": "https://ghcr.io",
+  "cloudsmith.tricej.us.kg": "https://docker.cloudsmith.io",
+  "ecr.tricej.us.kg": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
